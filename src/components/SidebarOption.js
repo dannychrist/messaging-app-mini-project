9e4f6c924f1b8ebf9b1a51f5028527db8f3de778
Daniel Christiansen <dannyjameschrist@gmail.com>
2022-03-07 16:31:55 -0500

    new changes

--- src/components/SidebarOption.js
+++ src/components/SidebarOption.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
+import { db } from '../firebase';
 
 const SidebarOption = ({ Icon, addChannelOption, title }) => {
 
   const addChannel = () => {
-    
+    const channelName = prompt('Please enter the channel name');
+
+    if (channelName) {
+      db.collection('rooms').add({
+        name: channelName
+      })
+    }
   }
 
   const selectChannel = () => {
