5cd944dc0c566a9fa5b2b462cc4a55dec7b5bdee
Daniel Christiansen <dannyjameschrist@gmail.com>
2022-03-07 15:19:30 -0500

    new changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import styled from 'styled-components';
 
 import Header from './components/Header';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className='App'>
       <Router>
         <>
-          <Routes>
-            <Route path='/' exact element={<Header />} />
-          </Routes>
+          <Header />
+          <AppBody>
+            <Sidebar />
+
+            <Routes>{/* <Route path='/' exact element={<Chat />} /> */}</Routes>
+          </AppBody>
         </>
       </Router>
     </div>
@@ -19,3 +24,8 @@ function App() {
 }
 
 export default App;
+
+const AppBody = styled.div`
+  display: flex;
+  height: 100vh;
+`;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Avatar } from '@material-ui/core';
-import { AccessTime } from '@material-ui/icons';
+import { AccessTime as AccessTimeIcon } from '@material-ui/icons';
+import { Search as SearchIcon } from '@material-ui/icons';
+import { HelpOutline as HelpOutlineIcon } from '@material-ui/icons';
 
 const Header = () => {
   return (
@@ -11,14 +13,19 @@ const Header = () => {
         <HeaderAvatar
         // TODO Add onClick
         />
-        <AccessTime />
+        <AccessTimeIcon />
       </HeaderLeft>
 
       {/* Header Search */}
-      <HeaderSearch></HeaderSearch>
+      <HeaderSearch>
+        <SearchIcon />
+        <input placeholder='Start searching...' />
+      </HeaderSearch>
 
       {/* Header Right */}
-      <HeaderRight></HeaderRight>
+      <HeaderRight>
+        <HelpOutlineIcon />
+      </HeaderRight>
     </HeaderContainer>
   );
 };
@@ -27,12 +34,63 @@ export default Header;
 
 const HeaderContainer = styled.div`
   display: flex;
+  position: fixed;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px 0;
+  background-color: var(--slack-color);
+  color: white;
 `;
 
-const HeaderLeft = styled.div``;
+const HeaderLeft = styled.div`
+  display: flex;
+  flex: 0.3;
+  align-items: center;
+  margin-left: 20px;
 
-const HeaderAvatar = styled(Avatar)``;
+  > .MuiSvgIcon-root {
+    margin-left: auto;
+    margin-right: 30px;
+  }
+`;
 
-const HeaderSearch = styled.div``;
+const HeaderAvatar = styled(Avatar)`
+  cursor: pointer;
 
-const HeaderRight = styled.div``;
+  :hover {
+    opacity: 0.8;
+  }
+`;
+
+const HeaderSearch = styled.div`
+  flex: 0.4;
+  opacity: 1;
+  border-radius: 6px;
+  background-color: #421f44;
+  text-align: center;
+  display: flex;
+  padding: 0 50px;
+  color: gray;
+  border: 1px gray solid;
+
+  > input {
+    background-color: transparent;
+    border: none;
+    text-align: center;
+    min-width: 30vw;
+    outline: 0;
+    color: white;
+  }
+`;
+
+const HeaderRight = styled.div`
+  flex: 0.3;
+  display: flex;
+  align-items: flex-end;
+
+  > .MuiSvgIcon-root {
+    margin-left: auto;
+    margin-right: 20px;
+  }
+`;
