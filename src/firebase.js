8195fbee065928201dbd748ed665b27116b7979a
Daniel Christiansen <dannyjameschrist@gmail.com>
2022-03-07 12:08:16 -0500

    new changes

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAmq_u2_U-GjDgCtMlTQ4zmY4QYAsPEQX4",
+  authDomain: "messaging-app-mini-project.firebaseapp.com",
+  projectId: "messaging-app-mini-project",
+  storageBucket: "messaging-app-mini-project.appspot.com",
+  messagingSenderId: "320185406318",
+  appId: "1:320185406318:web:7d801d003843fe2a5aa77f"
+};
